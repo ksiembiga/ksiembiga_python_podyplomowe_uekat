0e738e5ebcbba4898a6cccc5f8b32ecd380a38ba
Konrad Siembiga <k.siembiga@outlook.com>
2025-12-24 22:24:38 +0100

    Zadania 2 i 3 OOP poprawione pycodestyle

--- OOPzad2.py
+++ OOPzad2.py
@@ -60,30 +60,27 @@ class Student:
 
 
 #Biblioteki
-mbpkatowice = Library(city = 'Katowice', street='Chrobrego 2', zip_code = '40-871', open_hours= '8-16', phone='+32 555 555')
-mpbtychy = Library (city='Tychy', street= 'Piłsudskiego 10', zip_code='43-102', open_hours= '8-16', phone='+32 111 111' )
+mbpkatowice=Library(city ='Katowice', street='Chrobrego 2', zip_code='40-871', open_hours='8-16', phone='+32 555 555')
+mpbtychy=Library (city='Tychy', street='Piłsudskiego 10', zip_code='43-102', open_hours='8-16', phone='+32 111 111' )
 #Książki
-wladca_pierscieni = Book(title = 'Władca Pierscieni', library = mbpkatowice, publication_date='1955', author_name='J.R.R.', author_surname='Tolkien', number_of_pages='600')
-hobbit = Book(title = 'Hobbit', library=mbpkatowice, publication_date='1955', author_name='J.R.R.', author_surname='Tolkien', number_of_pages='200')
-historia_chorzowa = Book(title = 'Historia Chorzowa', library=mpbtychy, publication_date='2000', author_name='Jan', author_surname='Drabina', number_of_pages='330')
-Swiaty_tolkiena = Book(title = 'Światy Tolkiena', library=mbpkatowice, publication_date='2015', author_name='John', author_surname='Garth', number_of_pages='180')
-Niemiecki_nie_gryzie = Book(title = 'Niemiecki nie gryzie', library=mpbtychy, publication_date='2020', author_name='Autor', author_surname='Nieznany', number_of_pages='135')
+wladca_pierscieni=Book(title ='Władca Pierscieni', library=mbpkatowice, publication_date='1955', author_name='J.R.R.', author_surname='Tolkien', number_of_pages='600')
+hobbit=Book(title='Hobbit', library=mbpkatowice, publication_date='1955', author_name='J.R.R.', author_surname='Tolkien', number_of_pages='200')
+historia_chorzowa=Book(title ='Historia Chorzowa', library=mpbtychy, publication_date='2000', author_name='Jan', author_surname='Drabina', number_of_pages='330')
+Swiaty_tolkiena=Book(title ='Światy Tolkiena', library=mbpkatowice, publication_date='2015', author_name='John', author_surname='Garth', number_of_pages='180')
+Niemiecki_nie_gryzie=Book(title='Niemiecki nie gryzie', library=mpbtychy, publication_date='2020', author_name='Autor', author_surname='Nieznany', number_of_pages='135')
 #pracownicy
-jan_kowalski = Employee(first_name="Jan", last_name="Kowalski", hire_data="01-01-2012", birth_date="02-04-1977", phone='+32 123 456', street="Jodłowa 2", zip_code=' 11-111', city="Bytom")
-jan_nowak = Employee(first_name="Jan", last_name="Nowak", hire_data="05-10-2018", birth_date="12-06-1972", phone='+32 321 654', street="Główna 6", zip_code=' 22-121', city="Chorzów")
-tomasz_nowak = Employee(first_name="Tomasz", last_name="Nowak", hire_data="10-07-2003", birth_date="11-10-1968", phone='+32 333 333', street="Wschodnia 12/6", zip_code=' 44-144', city="Zabrze")
+jan_kowalski=Employee(first_name="Jan", last_name="Kowalski", hire_data="01-01-2012", birth_date="02-04-1977", phone='+32 123 456', street="Jodłowa 2", zip_code=' 11-111', city="Bytom")
+jan_nowak=Employee(first_name="Jan", last_name="Nowak", hire_data="05-10-2018", birth_date="12-06-1972", phone='+32 321 654', street="Główna 6", zip_code=' 22-121', city="Chorzów")
+tomasz_nowak=Employee(first_name="Tomasz", last_name="Nowak", hire_data="10-07-2003", birth_date="11-10-1968", phone='+32 333 333', street="Wschodnia 12/6", zip_code=' 44-144', city="Zabrze")
 #studenci
-Adam_Niezgodka = Student(first_name="Adam", last_name="Niezgodka", )
-Ambrozy_Kleks = Student(first_name="Ambrozy", last_name="Kleks", )
-Filip_Golarz = Student(first_name="Filip", last_name="Golarz", )
+Adam_Niezgodka=Student(first_name="Adam", last_name="Niezgodka", )
+Ambrozy_Kleks=Student(first_name="Ambrozy", last_name="Kleks", )
+Filip_Golarz=Student(first_name="Filip", last_name="Golarz", )
 #zamówienia
-zamowienie1 = Order(employee= jan_nowak.first_name + jan_nowak.last_name, student= Filip_Golarz.first_name + Filip_Golarz.last_name, books= wladca_pierscieni.title + hobbit.title, order_date='2-2-2025')
-zamowienie2 = Order(employee=jan_kowalski.first_name + jan_kowalski.last_name, student=Ambrozy_Kleks.first_name + Ambrozy_Kleks.last_name, books= historia_chorzowa.title + Niemiecki_nie_gryzie.title, order_date= '4-2-2026')
+zamowienie1=Order(employee=jan_nowak.first_name + jan_nowak.last_name, student=Filip_Golarz.first_name + Filip_Golarz.last_name, books= wladca_pierscieni.title + hobbit.title, order_date='2-2-2025')
+zamowienie2=Order(employee=jan_kowalski.first_name + jan_kowalski.last_name, student=Ambrozy_Kleks.first_name + Ambrozy_Kleks.last_name, books=historia_chorzowa.title + Niemiecki_nie_gryzie.title,
+                    order_date='4-2-2026')
 
 
 print(zamowienie1)
 print(zamowienie2)
-
-
-
-
--- OOPzad3.py
+++ OOPzad3.py
@@ -1,7 +1,8 @@
 import Property_classes
 
-class Property :
-    def __init__(self,area, rooms:int, price, address):
+
+class Property:
+    def __init__(self, area, rooms: int, price, address):
         self.area = area
         self.rooms = rooms
         self.price = price
@@ -9,7 +10,7 @@ class Property :
 
 
 class House(Property):
-    def __init__(self,area, rooms, price, address, plot:int):
+    def __init__(self, area, rooms, price, address, plot: int):
         self.area = area
         self.rooms = rooms
         self.price = price
@@ -18,6 +19,8 @@ class House(Property):
 
     def __str__(self):
         return f'Powierzchnia domu: {self.area}, pokoje: {self.rooms}, cena: {self.price}, adres:  {self.address}, powierzchnia działki:  {self.plot}'
+
+
 class Flat(Property):
     def __init__(self, area, rooms, price, address, floor):
         self.area = area
@@ -30,9 +33,8 @@ class Flat(Property):
         return f'Powierzchnia domu: {self.area}, pokoje: {self.rooms}, cena: {self.price}, adres:  {self.address}, ilośc poziomów:  {self.floor}'
 
 
-
-domek_w_gorach = House(area='200', rooms=8, price= '700000', address='ul. lesna12, ustron', plot=900)
-mieszkanie_w_miescie = Flat(area='80', rooms = 4, price ='450000', address='ul. Miejska, Bielsko-Biała', floor='1')
+domek_w_gorach=House(area='200', rooms=8, price='700000', address='ul. lesna12, ustron', plot=900)
+mieszkanie_w_miescie=Flat(area='80', rooms=4, price ='450000', address='ul. Miejska, Bielsko-Biała', floor='1')
 
 print(domek_w_gorach)
 print(mieszkanie_w_miescie)
